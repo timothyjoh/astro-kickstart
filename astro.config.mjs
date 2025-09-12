55f6cc6d7478336681c0c91055d4d38ec45b5fa3
Timothy Johnson <jhnsntmthy@gmail.com>
2025-09-12 14:05:05 -0400

    blog content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel'
+import mdx from '@astrojs/mdx'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
